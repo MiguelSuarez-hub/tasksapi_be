b70b1a80098d2edb8a06a76ccd71e8e0da6659f3
Mike <miguel.suarezf05@gmail.com>
2023-05-30 17:44:30 -0500

    added functionality for deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config/dist';
 
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1/tasks'),
-    TasksModule],
+    TasksModule,
+    ConfigModule.forRoot()],
 })
 export class AppModule {}
