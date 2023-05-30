48735f23e80378d5a6dd24d9630917ed2d90fc49
Mike <miguel.suarezf05@gmail.com>
2023-05-30 10:03:15 -0500

    developed backend, with mongoose and nest

---
+++ src/schemas/task.schema.ts
@@ -0,0 +1,27 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from 'mongoose';
+
+export type TaskDocument = HydratedDocument<Task>;
+
+@Schema({
+  timestamps: true
+})
+
+export class Task {
+  @Prop({
+    unique: true,
+    required: true,
+    trim:true
+  })
+  title: string;
+  @Prop({
+    trim: true
+  })
+  description: string;
+  @Prop({
+    default: false
+  })
+  done: boolean;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,48 @@
+import { Controller, Get, Post, Delete, Put, Body, Param, ConflictException, NotFoundException, HttpCode } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from 'src/dto/create-task.dto';
+import { UpdateTaskDto } from 'src/dto/update-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const task = await this.taskService.findOne(id);        
+    if (!task) throw new NotFoundException('Task not found');
+    return task;
+  }
+
+  @Post()
+  async create(@Body() body: CreateTaskDto) {
+    try {
+      return await this.taskService.create(body);
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Task already exists')
+      }
+      throw error;
+    }
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async delete(@Param('id') id: string) {  
+    const task = await this.taskService.delete(id);        
+    if (!task) throw new NotFoundException('Task not found');
+    return task;  
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() body: UpdateTaskDto) {
+    const task = await this.taskService.update(id, body);       
+    if (!task) throw new NotFoundException('Task not found');
+    return task;  
+  }
+}
